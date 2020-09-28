736bb6a72c5528f2c30dc5b6cf887dc07d0e0bfd
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-09-28 14:28:39 -0500

    case correction -- generate only small case links

--- docs/index.js
+++ docs/index.js
@@ -40,7 +40,9 @@ let geturl = () => {
 
 let getrandom = () => {
     let text = "";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    // let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
 
     for (let i = 0; i < 3; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
